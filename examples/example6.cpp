b8a5e445bc32927491205b715fd94d182656d908
Drwalin <34726800+Drwalin@users.noreply.github.com>
2023-10-31 17:22:06 +0100

    Correct example6.

--- examples/example6.cpp
+++ examples/example6.cpp
@@ -3,6 +3,9 @@
 #include <thread>
 #include <atomic>
 
+#include <unistd.h>
+#include <signal.h>
+
 #include "../include/icon6/Host.hpp"
 #include "../include/icon6/MethodInvocationEnvironment.hpp"
 
@@ -73,14 +76,6 @@ int main()
 {
 	uint16_t port1 = 4000, port2 = 4001;
 
-	icon6::Initialize();
-
-	auto host1 = new icon6::Host(port1);
-	auto host2 = new icon6::Host(port2);
-
-	host1->SetMessagePassingEnvironment(mpe);
-	host2->SetMessagePassingEnvironment(mpe2);
-
 	mpe2->RegisterClass<TestClass>("TestClass", nullptr);
 	mpe2->RegisterMemberFunction<TestClass>("TestClass", "Method",
 											&TestClass::Method);
@@ -90,111 +85,132 @@ int main()
 										mpe2->GetClassByName("TestClass"));
 	mpe2->RegisterMemberFunction<TestClass>("TestClass", "Method2",
 											&TestClass::Method2);
-
 	uint64_t obj[2];
 	mpe2->CreateLocalObject<void>("TestClass", obj[0]);
 	mpe2->CreateLocalObject<void>("InheritedClass", obj[1]);
 
-	printf(" TestClass: %lu\n", obj[0]);
-	printf(" InheritedClass: %lu\n", obj[1]);
+	pid_t c_pid = fork();
 
-	host1->RunAsync();
-	host2->RunAsync();
+	icon6::Initialize();
 
-	auto P1 = host1->ConnectPromise("localhost", 4001);
-	P1.wait();
-	auto p1 = P1.get();
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
-	if (p1 != nullptr) {
-		mpe->CallInvoke(p1, 0,
-						icon6::OnReturnCallback::Make<std::vector<int>>(
-							[](icon6::Peer *peer, icon6::Flags flags,
-							   std::vector<int> data) -> void {
-								printf(" TestClass::Method return callback:");
-								for (int i : data)
-									printf(" %i", i);
-								printf("\n");
-							},
-							[](icon6::Peer *peer) -> void {
-								printf(" void(void) timeout\n");
-							},
-							10000, p1),
-						obj[0], "Method", 123);
-
-		mpe->CallInvoke(p1, 0,
-						icon6::OnReturnCallback::Make<std::vector<int>>(
-							[](icon6::Peer *peer, icon6::Flags flags,
-							   std::vector<int> &data) -> void {
-								printf(
-									" InheritedClass::Method return callback:");
-								for (int i : data)
-									printf(" %i", i);
-								printf("\n");
-							},
-							[](icon6::Peer *peer) -> void {
-								printf(" void(void) timeout\n");
-							},
-							10000, p1),
-						obj[1], "Method", 4567);
-
-		mpe->CallInvoke(p1, 0,
-						icon6::OnReturnCallback::Make<std::string>(
-							[](icon6::Peer *peer, icon6::Flags flags,
-							   std::string data) -> void {
-								printf(
-									" TestClass::Method2 return callback: %s\n",
-									data.c_str());
-							},
-							[](icon6::Peer *peer) -> void {
-								printf(" void(void) timeout\n");
-							},
-							10000, p1),
-						obj[0], "Method2", "asdf", 666);
-
-		mpe->CallInvoke(
-			p1, 0,
-			icon6::OnReturnCallback::Make<std::string>(
-				[](icon6::Peer *peer, icon6::Flags flags,
-				   std::string data) -> void {
-					printf(" InheritedClass::Method2 return callback: %s\n",
-						   data.c_str());
-				},
-				[](icon6::Peer *peer) -> void {
-					printf(" void(void) timeout\n");
-				},
-				10000, p1),
-			obj[1], "Method2", "qwerty", 999);
-
-		mpe->CallInvoke(p1, 0,
-						icon6::OnReturnCallback::Make(
-							[](icon6::Peer *peer, icon6::Flags flags) -> void {
-								printf(" TestClass::Method3 return callback\n");
-							},
-							[](icon6::Peer *peer) -> void {
-								printf(" TestClass::Method3 timeout\n");
-							},
-							10000, p1),
-						obj[1], "Method3");
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
-
-		p1->Disconnect();
+	if (c_pid == -1) {
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (c_pid <= 0) {
+		auto host2 = new icon6::Host(port2);
+		host2->SetMessagePassingEnvironment(mpe2);
+		host2->RunAsync();
+		std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+		host2->Stop();
+		host2->WaitStop();
+		delete host2;
 	} else {
-		throw "Didn't connect to peer.";
-	}
 
-	host2->Stop();
-	host1->WaitStop();
-	host2->WaitStop();
+		auto host1 = new icon6::Host(port1);
+
+		host1->SetMessagePassingEnvironment(mpe);
+
+		printf(" TestClass: %lu\n", obj[0]);
+		printf(" InheritedClass: %lu\n", obj[1]);
+
+		host1->RunAsync();
+
+		auto P1 = host1->ConnectPromise("127.0.0.1", port2);
+		P1.wait();
+		auto p1 = P1.get();
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+		if (p1 != nullptr) {
+			mpe->CallInvoke(p1, 0,
+							icon6::OnReturnCallback::Make<std::vector<int>>(
+								[](icon6::Peer *peer, icon6::Flags flags,
+								   std::vector<int> data) -> void {
+									printf(
+										" TestClass::Method return callback:");
+									for (int i : data)
+										printf(" %i", i);
+									printf("\n");
+								},
+								[](icon6::Peer *peer) -> void {
+									printf(" void(void) timeout\n");
+								},
+								10000, p1),
+							obj[0], "Method", 123);
+
+			mpe->CallInvoke(
+				p1, 0,
+				icon6::OnReturnCallback::Make<std::vector<int>>(
+					[](icon6::Peer *peer, icon6::Flags flags,
+					   std::vector<int> &data) -> void {
+						printf(" InheritedClass::Method return callback:");
+						for (int i : data)
+							printf(" %i", i);
+						printf("\n");
+					},
+					[](icon6::Peer *peer) -> void {
+						printf(" void(void) timeout\n");
+					},
+					10000, p1),
+				obj[1], "Method", 4567);
+
+			mpe->CallInvoke(
+				p1, 0,
+				icon6::OnReturnCallback::Make<std::string>(
+					[](icon6::Peer *peer, icon6::Flags flags,
+					   std::string data) -> void {
+						printf(" TestClass::Method2 return callback: %s\n",
+							   data.c_str());
+					},
+					[](icon6::Peer *peer) -> void {
+						printf(" void(void) timeout\n");
+					},
+					10000, p1),
+				obj[0], "Method2", "asdf", 666);
+
+			mpe->CallInvoke(
+				p1, 0,
+				icon6::OnReturnCallback::Make<std::string>(
+					[](icon6::Peer *peer, icon6::Flags flags,
+					   std::string data) -> void {
+						printf(" InheritedClass::Method2 return callback: %s\n",
+							   data.c_str());
+					},
+					[](icon6::Peer *peer) -> void {
+						printf(" void(void) timeout\n");
+					},
+					10000, p1),
+				obj[1], "Method2", "qwerty", 999);
+
+			mpe->CallInvoke(
+				p1, 0,
+				icon6::OnReturnCallback::Make(
+					[](icon6::Peer *peer, icon6::Flags flags) -> void {
+						printf(" TestClass::Method3 return callback\n");
+					},
+					[](icon6::Peer *peer) -> void {
+						printf(" TestClass::Method3 timeout\n");
+					},
+					10000, p1),
+				obj[1], "Method3");
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+			p1->Disconnect();
+		} else {
+			throw "Didn't connect to peer.";
+		}
+
+		host1->WaitStop();
+
+		delete host1;
+		delete mpe;
+
+		kill(c_pid, SIGKILL);
+	}
+	delete mpe2;
 
 	icon6::Deinitialize();
-
-	delete host1;
-	delete host2;
-	delete mpe;
-	delete mpe2;
 
 	return 0;
 }
