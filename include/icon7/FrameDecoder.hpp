854c71217ca9f85cd4f47a9d6a66e8e29d3c6327
Drwalin <34726800+Drwalin@users.noreply.github.com>
2024-01-18 18:51:01 +0100

    Separate frame decoding code into FrameDecoder class.

---
+++ include/icon7/FrameDecoder.hpp
@@ -0,0 +1,56 @@
+/*
+ *  This file is part of ICon7.
+ *  Copyright (C) 2023-2024 Marek Zalewski aka Drwalin
+ *
+ *  ICon7 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICon7 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ICON7_FRAME_DECODER_HPP
+#define ICON7_FRAME_DECODER_HPP
+
+#include <cinttypes>
+
+#include <vector>
+#include <atomic>
+#include <memory>
+#include <queue>
+
+#include "Flags.hpp"
+#include "FramingProtocol.hpp"
+#include "ByteReader.hpp"
+#include "Command.hpp"
+
+namespace icon7
+{
+class Peer;
+
+class FrameDecoder
+{
+public:
+	FrameDecoder();
+
+	void Restart();
+	void PushData(uint8_t *data, uint32_t length,
+				  void (*onPacket)(std::vector<uint8_t> &buffer,
+								   uint32_t headerSize, void *userPtr),
+				  void *userPtr);
+
+private:
+	std::vector<uint8_t> buffer;
+	uint32_t headerSize;
+	uint32_t frameSize;
+};
+} // namespace icon7
+
+#endif
---
+++ src/FrameDecoder.cpp
@@ -0,0 +1,87 @@
+/*
+ *  This file is part of ICon7.
+ *  Copyright (C) 2023-2024 Marek Zalewski aka Drwalin
+ *
+ *  ICon7 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  ICon7 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <cstring>
+
+#include "../include/icon7/Host.hpp"
+#include "../include/icon7/RPCEnvironment.hpp"
+#include "../include/icon7/Command.hpp"
+#include "../include/icon7/FramingProtocol.hpp"
+
+#include "../include/icon7/FrameDecoder.hpp"
+
+namespace icon7
+{
+FrameDecoder::FrameDecoder() { Restart(); }
+
+void FrameDecoder::PushData(uint8_t *data, uint32_t length,
+							void (*onPacket)(std::vector<uint8_t> &buffer,
+											 uint32_t headerSize,
+											 void *userPtr),
+							void *userPtr)
+{
+	while (length) {
+		if (headerSize == 0) {
+			headerSize = FramingProtocol::GetPacketHeaderSize(data[0]);
+		}
+		if (buffer.size() < headerSize) {
+			uint32_t bytes =
+				std::min<uint32_t>(length, headerSize - buffer.size());
+			buffer.insert(buffer.end(), data, data + bytes);
+			length -= bytes;
+			data += bytes;
+		}
+		if (buffer.size() < headerSize) {
+			return;
+		}
+		if (buffer.size() == headerSize) {
+			frameSize = headerSize + FramingProtocol::GetPacketBodySize(
+										 buffer.data(), headerSize);
+			buffer.reserve(frameSize);
+		}
+
+		if (buffer.size() < frameSize) {
+			uint32_t bytes =
+				std::min<uint32_t>(length, frameSize - buffer.size());
+			buffer.insert(buffer.end(), data, data + bytes);
+			length -= bytes;
+			data += bytes;
+		}
+
+		if (buffer.size() == frameSize) {
+			if (onPacket) {
+				onPacket(buffer, headerSize, userPtr);
+			}
+			Restart();
+		} else if (buffer.size() >= frameSize) {
+			DEBUG("Error, FrameDecoder::PushData push to frame more than frame "
+				  "size was.");
+			throw;
+		} else {
+			continue;
+		}
+	}
+}
+
+void FrameDecoder::Restart()
+{
+	frameSize = 0;
+	headerSize = 0;
+	buffer.clear();
+}
+} // namespace icon7
