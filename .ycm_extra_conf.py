3dcfaf7cc40aaa4a1398d9fdaf08144ce2f3e954
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-04-22 12:57:45 +0200

    Update bitscpp. Improve CMakeLists.txt.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -2,6 +2,5 @@ def Settings( **kwargs ):
   return {
     'flags': ['-x', 'c++', '-Wall', '-pedantic', '-Isrc', '-Ibitscpp/include',
     '-Iinclude', '-IuSockets/src/libusockets.h',
-    '-IuSockets/src',
-    '-IGameNetworkingSockets/include', '-std=c++20', '-I/usr/include'],
+    '-IuSockets/src', '-std=c++20', '-I/usr/include'],
   }
