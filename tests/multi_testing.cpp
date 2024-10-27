a3371f979f6c11c6afb6b4d71b897c23539458b6
Drwalin <marek.zalewski.drwalin@gmail.com>
2024-10-27 19:58:22 +0100

    Use returnId per Peer for OnReturnedCallback instead of global id. Issue #29

--- tests/multi_testing.cpp
+++ tests/multi_testing.cpp
@@ -31,26 +31,34 @@ int Mul(int a, int b)
 int main(int argc, char **argv)
 {
 	const ArgsParser args(argc, argv);
-	
+
 	icon7::Initialize();
 
 	int notPassedTests = 0;
 	const int testsCount = args.GetInt({"-tests", "-t"}, 1, 1, 1000000000);
-	const int testsPerHostsPairCount = args.GetInt({"-pairs-per-test", "-p"}, 7, 1, 1000000000);
-	const int sendsMoreThanCalls = args.GetInt({"-sends-per-send", "-ss"}, 117, 2, 1000000000);
+	const int testsPerHostsPairCount =
+		args.GetInt({"-pairs-per-test", "-p"}, 7, 1, 1000000000);
+	const int sendsMoreThanCalls =
+		args.GetInt({"-sends-per-send", "-ss"}, 117, 2, 1000000000);
 	const int totalSends = args.GetInt({"-sends", "-s"}, 371, 1, 1000000000);
-	const int connectionsCount = args.GetInt({"-connections", "-con"}, 171, 1, 1000000);
-	int delayBetweeEachTotalSendMilliseconds = args.GetInt({"-delay", "-d"}, 0, 0, 10000);
-	const int64_t maxWaitAfterPayloadDone =  args.GetInt({"-max-wait-after-payload"}, 3*60*1000, 0, 1000ll*3600ll*24ll*365ll);
+	const int connectionsCount =
+		args.GetInt({"-connections", "-con"}, 171, 1, 1000000);
+	int delayBetweeEachTotalSendMilliseconds =
+		args.GetInt({"-delay", "-d"}, 0, 0, 10000);
+	const int64_t maxWaitAfterPayloadDone =
+		args.GetInt({"-max-wait-after-payload"}, 3 * 60 * 1000, 0,
+					1000ll * 3600ll * 24ll * 365ll);
 	const int useSSL = args.GetFlag({"-ssl"});
-	
+
 	uint16_t port = args.GetInt({"-port"}, 7312, 1, 65535);
-	
+
 	bool printMoreStats = args.GetFlag({"-verbose-stats", "-stats"});
-	
-	const int serializeSendsModulo = args.GetInt({"-serialize-modulo"}, 5, 1, 1000000);
-	const int serializeSendsFract = args.GetInt({"-serialize-load"}, 4, 1, 1000000);
-	
+
+	const int serializeSendsModulo =
+		args.GetInt({"-serialize-modulo"}, 5, 1, 1000000);
+	const int serializeSendsFract =
+		args.GetInt({"-serialize-load"}, 4, 1, 1000000);
+
 	const bool alwaysReturn0 = args.GetFlag({"-return-zero"});
 
 	int64_t toReturnCount = 0;
@@ -78,7 +86,8 @@ int main(int argc, char **argv)
 					"ECDHE-ECDSA-AES256-GCM-SHA384:"
 					"ECDHE-ECDSA-AES128-GCM-SHA256:"
 					"ECDHE-ECDSA-CHACHA20-POLY1305:"
-					"DHE-RSA-AES256-GCM-SHA384:");
+					"DHE-RSA-AES256-GCM-SHA384:",
+					1);
 		hosta->RunAsync();
 		auto listenFuture = hosta->ListenOnPort("127.0.0.1", port, icon7::IPv4);
 
@@ -87,7 +96,8 @@ int main(int argc, char **argv)
 		rpc2.RegisterMessage("mul", Mul);
 		icon7::uS::tcp::Host *hostb = new icon7::uS::tcp::Host();
 		hostb->SetRpcEnvironment(&rpc2);
-		hostb->Init(useSSL);
+		hostb->Init(useSSL, nullptr, nullptr, nullptr, nullptr, nullptr,
+					nullptr, 1);
 		hostb->RunAsync();
 
 		listenFuture.wait_for(std::chrono::milliseconds(150));
@@ -201,8 +211,8 @@ int main(int argc, char **argv)
 
 				for (int k = 0; k < totalSends; ++k) {
 					if (k > 0) {
-					std::this_thread::sleep_for(std::chrono::microseconds(
-						delayBetweeEachTotalSendMilliseconds*500ll));
+						std::this_thread::sleep_for(std::chrono::microseconds(
+							delayBetweeEachTotalSendMilliseconds * 500ll));
 					}
 					for (auto p : validPeers) {
 						auto peer = p.get();
@@ -230,7 +240,7 @@ int main(int argc, char **argv)
 					}
 					commandsBuffer->FlushBuffer();
 					std::this_thread::sleep_for(std::chrono::microseconds(
-						delayBetweeEachTotalSendMilliseconds*500ll));
+						delayBetweeEachTotalSendMilliseconds * 500ll));
 
 					for (int l = 0; l < sendsMoreThanCalls - 1; ++l) {
 						if (l % serializeSendsModulo < serializeSendsFract) {
@@ -267,7 +277,8 @@ int main(int argc, char **argv)
 					   _E - beginingBeforeConnectingInIteration)
 					   .count();
 			kops = (totalReceived + totalReturned + received.load() +
-					returned.load()) / _sec / 1000.0;
+					returned.load()) /
+				   _sec / 1000.0;
 			mibps = kops * sendFrameSize * 1000.0 / 1024.0 / 1024.0;
 			LOG_INFO("Total throughput: %f Kops (%f MiBps)", kops, mibps,
 					 sumTim * 1000.0 / ((double)returned.load()));
@@ -296,15 +307,17 @@ int main(int argc, char **argv)
 				LOG_INFO("Waiting to finish message transmission");
 			}
 
-			uint32_t oldReceived = received.load(), oldReturned = returned.load();
+			uint32_t oldReceived = received.load(),
+					 oldReturned = returned.load();
 			auto startWaitTimepoint = std::chrono::steady_clock::now();
-			for (int64_t i = 0; i < maxWaitAfterPayloadDone*100; ++i) {
+			for (int64_t i = 0; i < maxWaitAfterPayloadDone * 100; ++i) {
 				if (received >= sent && returned >= toReturnCount) {
 					break;
 				}
-				if (oldReceived != received.load() || oldReturned != returned.load()) {
+				if (oldReceived != received.load() ||
+					oldReturned != returned.load()) {
 					const auto now = std::chrono::steady_clock::now();
-					if ((now-startWaitTimepoint) > std::chrono::seconds(10)) {
+					if ((now - startWaitTimepoint) > std::chrono::seconds(10)) {
 						notPassedTests++;
 						break;
 					}
@@ -332,7 +345,7 @@ int main(int argc, char **argv)
 			if (sent < 8) {
 				notPassedTests++;
 			}
-			
+
 			totalSent += sent;
 			totalReceived += received;
 			totalReturned += returned;
