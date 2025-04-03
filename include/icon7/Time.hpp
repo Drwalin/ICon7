a4eb4fce314741cf88ad0ea615f19d9d1c6d50d6
Drwalin <34726800+Drwalin@users.noreply.github.com>
2025-04-04 01:08:42 +0200

    Add simplified time scales (seconds, milliseconds, microseconds and nanoseconds).

--- include/icon7/Time.hpp
+++ include/icon7/Time.hpp
@@ -17,7 +17,11 @@ namespace time
 uint64_t GetTemporaryTimestamp();
 uint64_t TemporaryTimestampToTimestamp(uint64_t tmpTimestamp);
 uint64_t GetTimestamp();
-uint64_t DeltaNsBetweenTimestamps(uint64_t begin, uint64_t end);
+int64_t DeltaNsBetweenTimestamps(uint64_t begin, uint64_t end);
+double DeltaSecBetweenTimestamps(uint64_t begin, uint64_t end);
+double DeltaMSecBetweenTimestamps(uint64_t begin, uint64_t end);
+double DeltaUSecBetweenTimestamps(uint64_t begin, uint64_t end);
+double NanosecondsToSeconds(int64_t ns);
 std::string TimestampToString(uint64_t timestamp, int subsecondsDigits);
 std::string GetCurrentTimestampString(int subsecondsDigits);
 uint64_t StringToTimestamp(std::string str);
